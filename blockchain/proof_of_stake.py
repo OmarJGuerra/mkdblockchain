e46d0347356a43ae0cdd92e74b98b80d41b8e99d
Joel Rodriguez <thenerdyyeti@gmail.com>
2021-07-21 13:21:08 -0400

    changed test file to find issue with tree growth

--- blockchain/proof_of_stake.py
+++ blockchain/proof_of_stake.py
@@ -9,7 +9,7 @@ class ProofOfStake:
         self.set_genesis_node_stake()
 
     def set_genesis_node_stake(self):
-        genesis_public_key = open('blockchain/keys/genesisPublicKey.pem', 'r').read()  # added blockchain to file path
+        genesis_public_key = open('keys/genesisPublicKey.pem', 'r').read()  # added blockchain to file path
         self.stakers[genesis_public_key] = 10
 
     def update(self, public_key_string, stake):
