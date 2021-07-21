f4acf64d16e28b65dcc75491c1f614fb630309ba
Joel Rodriguez <thenerdyyeti@gmail.com>
2021-07-20 21:39:22 -0400

    modified csv output

--- blockchain/main.py
+++ blockchain/main.py
@@ -88,7 +88,7 @@ def run_sim_thread(test_num, num_clusters, num_nodes, forge_interval, dimensions
                     with open(f'branch_size_left_right_{test_num}.csv', mode='a') as branch_size:
                         branch_size_writer = csv.writer(branch_size, delimiter='.', quotechar='"',
                                                         quoting=csv.QUOTE_MINIMAL)
-                        branch_size_writer.writerow([cycles, node.cluster_id, node.node_id, left_size, right_size])
+                        branch_size_writer.writerow([cycles, node.cluster_id, node.node_id, node.blockchain.blocks.size, left_size, right_size])
                 for node in nodes_transacting:
                     transaction = SensorTransaction(node.wallet.public_key_string(), random.randint(0, 1000))
                     node.publish(transaction)
